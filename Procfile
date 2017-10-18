7610d4b2a59c46e3dbbdbc8c4f1aafcb2e0b1270
Jamu Kakar <jkakar@kakar.ca>
2017-10-17 21:39:38 -0700

    Load YAML files.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/dashi-api *.yaml
--- cmd/dashi-api/main.go
+++ cmd/dashi-api/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/jkakar/dashi"
 	"github.com/joeshaw/envdecode"
@@ -19,8 +21,14 @@ func main() {
 		log.Fatalf("loading config failed: %q", err)
 	}
 	manifest := &dashi.Manifest{}
-	if err := dashi.Unmarshal(data, manifest); err != nil {
-		log.Fatal(err)
+	for _, filename := range os.Args[1:] {
+		data, err = ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatalf("error reading file %s: %q", filename, err)
+		}
+		if err := dashi.Unmarshal(data, manifest); err != nil {
+			log.Fatal(err)
+		}
 	}
 	handler := dashi.NewSearchHandler(manifest)
 	s := &http.Server{
@@ -30,49 +38,3 @@ func main() {
 	log.Printf("listening on port %d", cfg.Port)
 	log.Fatalf("listen and serve failed: %q", s.ListenAndServe())
 }
-
-var data = []byte(`
-teams:
-    - name: Runtime
-      services:
-          - name: domain-funnel
-            dashboards:
-                - name: st-legacy
-                  env: common-runtime-st
-                  url: https://metrics.librato.com/s/spaces/351543?source=common-runtime-st-shared
-                - name: ie-legacy
-                  env: common-runtime-ie
-                  url: https://metrics.librato.com/s/spaces/351543?source=common-runtime-ie-shared
-                - name: va-legacy
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/351543?source=common-runtime-va-shared
-          - name: release-manager
-            dashboards:
-                - name: st01
-                  env: common-runtime-st
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-st01
-                - name: ie01
-                  env: common-runtime-ie
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-ie01
-                - name: ie02
-                  env: common-runtime-ie
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-ie02
-                - name: va01
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-va01
-                - name: va02
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-va02
-                - name: va03
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-va03
-                - name: va04
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-va04
-                - name: va05
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-va05
-                - name: va06
-                  env: common-runtime-va
-                  url: https://metrics.librato.com/s/spaces/429997?source=common-runtime-va06
-`)
